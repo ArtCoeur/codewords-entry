016df0febcb33f82b2d61702786048b2544694eb
Tim Rodger <tim.rodger@gmail.com>
2015-04-05 16:50:23 +0100

    Updates board fact generation

--- src/lib/board.js
+++ src/lib/board.js
@@ -5,11 +5,10 @@ var uniqid = require('uniqid');
  */
 exports.create = function(context, board_data, mime_type, callback) {
 
-    // generate a board id
-    var id = uniqid();
+    // generate a board id for each board received
 
     var fact = {
-        board: id,
+        board: uniqid(),
         name: 'board.new',
         data: {
             body: board_data,
@@ -17,7 +16,8 @@ exports.create = function(context, board_data, mime_type, callback) {
         }
     };
 
-    // pop the new-board fact onto the queue
+    // pop the board.new fact onto the queue
+    
     context.on('ready', function() {
         var pub = context.socket('PUB');
         pub.connect('events', function(){
